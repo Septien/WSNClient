75cf2afb49de1e7225e66dfed2bfc8d708d706c7
Jose Septien <jaseptienh@gmail.com>
2020-10-22 12:15:34 -0500

    Add interrupt signal handler.

--- transciever.py
+++ transciever.py
@@ -1,14 +1,29 @@
 # For using hardware interface, abilitate:
 # https://superuser.com/questions/1205126/device-dev-spidev-not-found
-from RFM69 import Radio, FREQ_915MHZ
+# System modules
+import signal, os
 import datetime
 import time
+
+# Radio and MQTT
+from RFM69 import Radio, FREQ_915MHZ
 import paho.mqtt.client as paho
 # pip install rpi-rfm69
 
 # Multithreading
 import threading
 import queue
+
+# Handle interruptions
+interrupt = False
+
+def handler(signum, frame):
+    """
+    Handle the KeyboardInterrupt signal
+    """
+    global interrupt
+    if signum == signal.SIGINT:
+        interrupt = True
 
 class RadioC:
     """
@@ -20,6 +35,7 @@ class RadioC:
         self.recipient_id = recipient_id
         self.radioOn = False
         self.initalizeLoRaRadio()
+        print(self.radio.read_temperature())
 
     def initalizeLoRaRadio(self):
         self.radio = Radio(FREQ_915MHZ, self.node_id, self.network_id, isHighPower=True, verbose=True)
@@ -42,17 +58,20 @@ class RadioThread(threading.Thread):
     Create a background thread for constantly listening incoming transmissions from
     the LoRa Radio.
     """
-    def __init__(self, radio, threadID, name, q, qLock):
+    def __init__(self, radio, threadID, name, q, qLock, exitQ, exitL):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
         self.queue = q
         self.qLock = qLock
+        self.exitQ = exitQ
+        self.exitL = exitL
         self.radio = radio
 
     def run(self):
         """ Run the thread """
-        while True:
+        finish = False
+        while not finish:
             time.sleep(1)
             packets = self.radio.getRFData()
             if packets == None or len(packets) == 0:
@@ -61,6 +80,12 @@ class RadioThread(threading.Thread):
                 self.qLock.acquire()
                 self.queue.put(packets.copy())
                 self.qLock.release()
+            # Process thread termination
+            self.exitL.acquire()
+            if not self.exitQ.empty():
+                e = self.exitQ.get()
+                finish = True
+            self.exitL.release()
 
 class LoRaMQTTClient:
     def __init__(self, node_id, network_id, recipient_id, broker, port, topic):
@@ -77,7 +102,9 @@ class LoRaMQTTClient:
         # For the thread
         self.queue = queue.Queue(100)
         self.lock = threading.Lock()
-        self.rThread = RadioThread(self.radio, 1, "RadioThread", self.queue, self.lock)
+        self.exitQ = queue.Queue(1)
+        self.exitL = threading.Lock()
+        self.rThread = RadioThread(self.radio, 1, "RadioThread", self.queue, self.lock, self.exitQ, self.exitL)
 
     # Functions for client
     def on_connect(self, client, userdata, flags, rc):
@@ -103,6 +130,7 @@ class LoRaMQTTClient:
         self.client.on_disconnect = self.on_disconnect
 
     def run(self):
+        global interrupt
         print ("Starting loop...")
 
         delay = 1
@@ -131,6 +159,30 @@ class LoRaMQTTClient:
             #print("Sending packet Hello")
             #self.client.publish(self.topic, "Hello")
             self.client.loop()
+            if interrupt:
+                break
+        self.disconnect()
+    
+    def disconnect(self):
+        """
+        Handle the end of program
+        """
+        # Handle all remained packets
+        self.lock.acquire()
+        while not self.queue.empty():
+            packets = self.queue.get()
+            self.client.publish(self.topic, " ".join(packets))
+        self.lock.release()
+        # Terminate thread
+        self.exitL.acquire()
+        self.exitQ.put(-1)
+        self.exitL.release()
+        self.rThread.join()
+        
+        # Terminate MQTT client
+        self.client.wait_for_publish()
+        self.client.loop()
+        self.client.disconnect()
 
 # Post quantum:
 # NEWHOPE-ECDSA-AES128-GCM-SHA256
