3b67215b6835a3e005b5b8053cb93ffd5727172b
Jose Septien <jaseptienh@gmail.com>
2020-12-08 11:40:54 -0600

    Modularized features.

--- cpahoClient.c
+++ cpahoClient.c
@@ -2,40 +2,88 @@
 #include <stdlib.h>
 #include <string.h>
 #include "MQTTClient.h"
+#include <pthread.h>
 
-#define ADDRESS     "192.168.1.99:1883"
-#define CLIENTID    "Test"
-#define TOPIC       "test/topic"
-#define PAYLOAD     "Hello there!"
-#define QOS         1
-#define TIMEOUT     10000L
+struct clientInfo {
+    char *address;
+    char *clientid;
+    char *topic;
+    char *payload;
+    int qos;
+    unsigned long int timeout;
+};
 
-int main(int argc, char **argv)
+struct threadData {
+    char data[100];
+    int out;
+    pthread_mutex_t dataMutex;
+    pthread_mutex_t outMutex;
+};
+
+/*
+*   Create the client structure with necessary data.
+*/
+void createClient(MQTTClient *client, struct clientInfo *cf, char *capath, char *group)
 {
-    MQTTClient client;
+    client = (MQTTClient *)malloc(sizeof(MQTTClient));
     MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
-    MQTTClient_message pubmsg = MQTTClient_message_initializer;
-    MQTTClient_deliveryToken token;
+    MQTTClient_SSLOptions ssl_opts = MQTTClient_SSLOptions_initializer;
     int rc;
 
-    MQTTClient_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
+    /* Configure SSL client options */
+    ssl_opts.enableServerCertAuth = 0;
+    ssl_opts.enabledGroups = group; /* Key exchange */
+    ssl_opts.CApath = capath;
+    conn_opts.ssl = &ssl_opts;
+
+    MQTTClient_create(client, cf->address, cf->clientid, MQTTCLIENT_PERSISTENCE_NONE, NULL);
     conn_opts.keepAliveInterval = 20;
     conn_opts.cleansession = 1;
 
-    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
+    if ((rc = MQTTClient_connect(*client, &conn_opts)) != MQTTCLIENT_SUCCESS)
     {
         printf("Failed to connect, return code %d\n", rc);
         exit(EXIT_FAILURE);
     }
+}
 
-    pubmsg.payload = PAYLOAD;
-    pubmsg.payloadlen = strlen(PAYLOAD);
-    pubmsg.qos = QOS;
+void delivereMessage(MQTTClient *client, struct clientInfo *cf , MQTTClient_deliveryToken *token)
+{
+    MQTTClient_message pubmsg = MQTTClient_message_initializer;
+    int rc;
+
+    pubmsg.payload = cf->payload;
+    pubmsg.payloadlen = strlen(cf->payload);
+    pubmsg.qos = cf->qos;
     pubmsg.retained = 0;
-    MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
-    printf("Waiting for up to %d seconds for publication of %s\non topic %s for client with ClientID: %s", (int)(TIMEOUT/1000), PAYLOAD, TOPIC, CLIENTID);
-    rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
-    printf("\nMessage with delivety token %d delivered\n", token);
+    MQTTClient_publishMessage(client, cf->topic, &pubmsg, token);
+    rc = MQTTClient_waitForCompletion(client, *token, cf->timeout);
+    printf("Message: %s sent.\n", (char *)pubmsg.payload);
+}
+
+int main(int argc, char **argv)
+{
+    struct clientInfo cf;
+    MQTTClient_deliveryToken token;
+    MQTTClient client;
+    int rc;
+
+    /* Configure client options */
+    cf.address = "ssl://192.168.1.99";
+    cf.clientid = "Test";
+    cf.topic = "test/topic";
+    cf.payload = "Hello there! From paho C";
+    cf.qos = 1;
+    cf.timeout = 10000L;
+
+    char *capath = "/home/jash/Documentos/Maestria/certs/pqcerts2/ca.crt";
+    char *group = "lightsaber";
+    createClient(&client, &cf, capath, group);
+
+    printf("Waiting for up to %d seconds for publication of %s\non topic %s for client with ClientID: %s.\n", (int)(cf.timeout/1000), cf.payload, cf.topic, cf.clientid);
+    delivereMessage(&client, &cf, &token);
+    
+    printf("\nMessage with delivery token %d delivered\n", token);
     MQTTClient_disconnect(client, 10000);
     MQTTClient_destroy(&client);
     return 0;
